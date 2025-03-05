e3fe120c9d27b46f151ffc0bc2c20351c1181af4
saloni mittal <salonimittal9027@gmail.com>
2025-03-05 12:47:50 +0530

    [saloni] Add: adding UC5 code into branch

--- BusinessLayer/Service/GreetingBL.cs
+++ BusinessLayer/Service/GreetingBL.cs
@@ -1,51 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using BusinessLayer.Interface;
 using RepositoryLayer.Entity;
 using RepositoryLayer.Interface;
+
 
 namespace BusinessLayer.Service
 {
     public class GreetingBL : IGreetingBL
     {
+
+
         IGreetingRL _greetingRL;
 
+        // constructor
         public GreetingBL(IGreetingRL greetingRL)
         {
             _greetingRL = greetingRL;
         }
 
-        public UserEntity SaveGreetings(string message)
+
+        public UserEntity GetGreetingById(int id)
         {
-            var result = _greetingRL.SaveGreetings(message);
+            var result = _greetingRL.GetGreetingById(id);
             return result;
         }
 
-
-
-        public string GetGreeting(string? firstName, string? lastName)
-        {
-            if (!string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName))
+        public UserEntity SaveGreetings(string message)
             {
-                return "Hello " + firstName + " " + lastName;
+                var result = _greetingRL.SaveGreetings(message);
+                return result;
             }
 
-            else if (!string.IsNullOrEmpty(firstName))
-            {
-                return "Hello " + firstName;
-            }
-            else if (!string.IsNullOrEmpty(lastName))
-            {
-                return "Hello " + lastName;
-            }
-            else
-            {
-                return "Hello World!";
-            }
 
+
+        
+            public string GetGreeting(string? firstName, string? lastName)
+            {
+
+                if (!string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName))
+                {
+                    return "Hello " + firstName + " " + lastName;
+                }
+
+                else if (!string.IsNullOrEmpty(firstName))
+                {
+                    return "Hello " + firstName;
+                }
+                else if (!string.IsNullOrEmpty(lastName))
+                {
+                    return "Hello " + lastName;
+                }
+                else
+                {
+                    return "Hello World!";
+                }
+
+            }
         }
     }
-}
--- RepositoryLayer/Interface/IGreetingRL.cs
+++ RepositoryLayer/Interface/IGreetingRL.cs
@@ -10,5 +10,6 @@ namespace RepositoryLayer.Interface
     public interface IGreetingRL
     {
         public UserEntity SaveGreetings(string message);
+       public  UserEntity GetGreetingById(int id);
     }
 }
